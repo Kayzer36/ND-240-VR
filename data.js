7e79f0a723fe6880e02688f9542fde0e13fc7caa
Kayzer36 <xkayzer36x@gmail.com>
2025-03-13 16:20:25 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.060717073383809606,
+        "pitch": -0.0012096633523164257,
+        "fov": 1.3227526039011228
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4580071539428374,
+          "pitch": 0.035835758965898634,
+          "rotation": 0,
+          "target": "0-02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17796441173289956,
+          "pitch": -0.14325563264887897,
+          "title": "Это я",
+          "text": "Обезьяна, я здесь просто стою"
+        }
+      ]
+    },
+    {
+      "id": "1-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.012284330593875126,
+        "pitch": 0.028759297725612143,
+        "fov": 1.3227526039011228
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5674004599952518,
+          "pitch": -0.0074902834390471185,
+          "rotation": 0,
+          "target": "1-03"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1801140969461219,
+          "pitch": -0.14961516703050037,
+          "title": "Здесь тоже я",
+          "text": "Это мой вид сбоку"
+        }
+      ]
+    }
+  ],
+  "name": "240 VR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
